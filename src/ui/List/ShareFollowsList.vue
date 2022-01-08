360d4efd40a9b05ba3ba7f0565c1fc03bf5b95e3
YuanQiii <1833386727@qq.com>
2022-01-08 17:30:25 +0800

    带修复@点击无效问题

--- src/ui/List/ShareFollowsList.vue
+++ src/ui/List/ShareFollowsList.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-05 10:22:45
- * @LastEditTime: 2022-01-07 15:04:45
+ * @LastEditTime: 2022-01-08 17:29:03
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vue-NeteaseCloudMusic\src\ui\List\ShareFollowsList.vue
@@ -9,7 +9,7 @@
 <template>
   <div class="share-follows-list">
     <div class="header">选择最近@的人或直接输入</div>
-    <div class="list" @click="getFollowNickname">
+    <div class="list" @click="4">
       <div
         class="item"
         v-for="(value, index) in follows"
