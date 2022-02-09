a4d5afcfebc053358219b824edf25c00f7bd0212
YuanQiii <1833386727@qq.com>
2022-02-09 17:59:45 +0800

    待完善轮播

--- src/components/recommend/NewAlbum.vue
+++ src/components/recommend/NewAlbum.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-07 16:42:11
- * @LastEditTime: 2022-02-09 17:56:57
+ * @LastEditTime: 2022-02-09 17:59:07
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vue-NeteaseCloudMusic\src\components\recommend\NewAlbum.vue
