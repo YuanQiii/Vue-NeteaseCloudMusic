f88137f8d738ef95e46118e4f056476e848def51
YuanQiii <1833386727@qq.com>
2022-02-09 17:59:21 +0800

    待完善轮播

--- src/components/recommend/NewAlbum.vue
+++ src/components/recommend/NewAlbum.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-07 16:42:11
- * @LastEditTime: 2022-02-07 17:47:54
+ * @LastEditTime: 2022-02-09 17:56:57
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vue-NeteaseCloudMusic\src\components\recommend\NewAlbum.vue
@@ -11,12 +11,14 @@
     <recommend-nav :title="title" :tabList="[]" :moreUrl="moreUrl" />
     <div class="main">
       <div class="content">
-        <div class="item" v-for="(value, index) in albumList">
-          <div class="bg"></div>
+        <div class="slider" :style="sliderStyle">
+          <div class="item" v-for="(value, index) in albumList">
+            <div class="bg"></div>
+          </div>
         </div>
       </div>
-      <div class="icon1"></div>
-      <div class="icon2"></div>
+      <div class="icon1" @click="last"></div>
+      <div class="icon2" @click="next"></div>
     </div>
   </div>
 </template>
@@ -36,19 +38,53 @@ export default {
       title: "新碟上架",
       moreUrl: "null",
       albumList: [],
+      marginLeft: 0,
+      transition: 1,
+      gap: 650,
+      currentIndex: 0,
+      clickEnable: true,
     };
+  },
+  computed: {
+    sliderStyle() {
+      return {
+        marginLeft: `${this.marginLeft}px`,
+        transition: `${this.transition}s`,
+      };
+    },
   },
   methods: {
     getTopAlbum() {
       albumNewestApi()
         .then((response) => {
           if (response["status"] == 200) {
-            this.albumList = response["data"]["albums"].slice(0, 10);
+            let temp1 = response["data"]["albums"].slice(0, 10);
+            let temp2 = response["data"]["albums"].slice(0, 5);
+            this.albumList = temp1.concat(temp2);
           }
         })
         .catch((error) => {
           console.log(error);
         });
+    },
+    last() {},
+    next() {
+      if (this.clickEnable) {
+        this.clickEnable = false;
+        this.currentIndex += 1;
+        this.marginLeft -= this.gap;
+        setTimeout(() => {
+          this.clickEnable = true;
+          if (this.currentIndex == 2) {
+            this.transition = 0;
+            this.marginLeft = 0;
+            this.currentIndex = 0;
+            setTimeout(() => {
+              this.transition = 1;
+            }, 100);
+          }
+        }, 1000);
+      }
     },
   },
 };
@@ -64,20 +100,23 @@ export default {
     background: #f5f5f5;
     border: 1px solid #ccc;
     margin-top: 20px;
+    position: relative;
 
     .content {
-      display: flex;
       width: 635px;
       height: 130px;
       background-color: rgb(177, 28, 28);
       padding: 10px;
-      .item {
-        margin-right: 15px;
-        .bg {
-          width: 115px;
-          height: 100px;
-          background: url(https://s2.music.126.net/style/web2/img/coverall.png);
-          background-position: 0 -570px;
+      .slider {
+        display: flex;
+        .item {
+          margin-right: 15px;
+          .bg {
+            width: 115px;
+            height: 100px;
+            background: url(https://s2.music.126.net/style/web2/img/coverall.png);
+            background-position: 0 -570px;
+          }
         }
       }
     }
@@ -87,8 +126,9 @@ export default {
       background-position: -260px -75px;
       width: 17px;
       height: 17px;
-      margin-top: 75px;
-      margin-left: -10px;
+      position: absolute;
+      top: 65px;
+      left: 0;
       cursor: pointer;
       &:hover {
         background-position: -280px -75px;
@@ -99,9 +139,10 @@ export default {
       background-position: -300px -75px;
       width: 17px;
       height: 17px;
-      margin-left: 645px;
-      margin-top: -17px;
       cursor: pointer;
+      position: absolute;
+      top: 65px;
+      left: 670px;
       &:hover {
         background-position: -320px -75px;
       }
