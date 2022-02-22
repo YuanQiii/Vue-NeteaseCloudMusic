b67b72c5a0cdc575a2adccd8b3961ed57efd65e0
YuanQiii <1833386727@qq.com>
2022-02-22 22:39:12 +0800

    待完善主播side

--- src/components/side/RecommendSinger.vue
+++ src/components/side/RecommendSinger.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-02-22 20:53:44
- * @LastEditTime: 2022-02-22 22:36:40
+ * @LastEditTime: 2022-02-22 22:38:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Vue-NeteaseCloudMusic\src\components\side\RcommendSinger.vue
